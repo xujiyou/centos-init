7c05fa0a7c06baf99b8a83807b3fd14ad9acc8aa
xujiyou <xujiyou555@gmail.com>
2020-07-14 19:03:33 +0800

    fix more bug

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-SERVERS="192.168.225.2 192.168.225.3 192.168.225.4 192.168.225.5 192.168.225.6 192.168.225.7 192.168.225.8 192.168.225.9"
+SERVERS="192.168.225.2 192.168.225.3 192.168.225.4 192.168.225.5 192.168.225.6 192.168.225.7 192.168.225.8 192.168.225.9 192.168.225.10"
 PASSWORD="kf-2020"
 
 # 将脚本拷贝到全部机器
@@ -20,7 +20,7 @@ ssh_exec_to_other() {
   ID="$(echo $1 | awk -F. '{print $NF}')"
   expect -c "
   set timeout 30;
-  ssh root@$1 \"bash /root/init.sh server-0$((ID-1))\"
+  spawn ssh root@$1 'bash /root/init.sh server-0$((ID-1))'
   expect {
     *password:* {send $PASSWORD\r; exp_continue;}
     *(yes/no)?* {send yes\r; exp_continue;}
@@ -43,7 +43,7 @@ scp_copy_to_all
 ssh_exec_nopass_to_other() {
   expect -c "
   set timeout 30;
-  ssh root@$1 \"bash /root/nopass-login.sh\"
+  spawn ssh root@$1 'bash /root/nopass-login.sh'
   expect {
     *password:* {send $PASSWORD\r; exp_continue;}
     *(yes/no)?* {send yes\r; exp_continue;}
